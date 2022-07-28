63e1b4978859c3b673109007ab30b5a8a54d21bc
skm0626 <skm0626@kookmin.ac.kr>
2022-07-28 19:08:54 +0900

    connet django and mysql

---
+++ algomoa/algomoa/my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # 사용할 DB의 종류
+        'NAME': 'algomoa_test',    # DB 이름
+        'USER': 'root',       # DB 계정 이름
+        'PASSWORD': 'rudalsdl!!99', # DB 계정의 패스워드
+        'HOST': 'localhost',    # IP
+        'PORT': '3306',         # 포트 번호
+    }
+}
+
+SECRET_KEY = 'django-insecure-k73n-hnnzly2wn5192$_p%)tt4@h6t8d-bu)*uid#ab6lllcxc'
